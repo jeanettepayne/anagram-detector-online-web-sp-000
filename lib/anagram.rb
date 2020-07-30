46c6bf29b916fd883b6408fa7847b93f7745cbd6
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-30 20:26:24 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_reader :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(arr)
+    binding.pry
+    
+  end
+  
+end
