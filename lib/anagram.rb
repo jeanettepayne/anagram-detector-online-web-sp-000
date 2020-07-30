ebcabf67da860e99b7db33fb3aa81192ca179090
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-30 21:09:27 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ class Anagram
   end
   
   def match(arr)
-    new_word = self.split(//).sort
+    # new_word = self.split(//).sort
     # new_arr = arr.map{|test_word| test_word.split(//).sort}
     arr.keep_if{|test_word| self.split(//).sort == test_word.split(//).sort}
         
