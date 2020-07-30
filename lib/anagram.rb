78336c3faff8250f8b7345a12d902eeddaca5642
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-30 20:45:45 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,8 +10,8 @@ class Anagram
   
   def match(arr)
     arr.map do |test_word| 
-      new_arr test_word.split(//).sort.compact
-      # == self.to_s.split(//).sort
+      if test_word.split(//).sort == self.to_s.split(//).sort
+        test_word.join
       
       
   end
